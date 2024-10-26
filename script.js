0ab24a89c8eeee4168a80778e49a69c06ead234e
keinyer <keinyeryguaran@gmail.com>
2024-10-26 11:22:08 -0400

    contador de numeros pares e impares

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const first = (second) => {
     
     for (var numero = 0; numero <= 20; numero++) {
         if (numero % 2 === 0) {
-            console.log(numero,"es par")
+            console.log(`El numero ${numero} es par`)
         }
         if (numero % 2 !== 0) {
             console.log(`El numero ${numero} es impar`)
