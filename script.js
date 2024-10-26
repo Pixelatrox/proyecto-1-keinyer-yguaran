c60109d14505c2066319f1db33d21303567223b4
keinyer <keinyeryguaran@gmail.com>
2024-10-26 11:48:43 -0400

    commitFinal

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("hola mundo");
-const first = (second) => {
+function numerosParesImpares() {
     
     for (var numero = 0; numero <= 20; numero++) {
         if (numero % 2 === 0) {
@@ -8,8 +8,9 @@ const first = (second) => {
         if (numero % 2 !== 0) {
             console.log(`El numero ${numero} es impar`)
         }
-        console.log(`El numero ${numero} es ${numero % 2 == 0 ? 'par' : 'impar'} :)`)
+       // console.log(`El numero ${numero} es ${numero % 2 == 0 ? 'par' : 'impar'} :)`)
     }
 }
 
-first()
+//let buton=document.queryselector("dictar")
+//first()
